559e66a7b77fa0aa2c572a84ee8fd3a418114c35
Alex Wohlbruck <alexwohlbruck@gmail.com>
2023-10-18 22:32:34 -0400

    Rework image gallery

--- script.js
+++ script.js
@@ -2,23 +2,23 @@
   const images = [
     {
       src: 'chop.jpg',
-      aspectRatio: 4/5,
+      aspectRatio: 4 / 5,
     },
     {
       src: 'creep.jpg',
-      aspectRatio: 4/5,
+      aspectRatio: 4 / 5,
     },
     {
       src: 'iloveme.jpg',
-      aspectRatio: 4/5,
+      aspectRatio: 4 / 5,
     },
     {
       src: 'melodrama.jpg',
-      aspectRatio: 4/5,
+      aspectRatio: 4 / 5,
     },
     {
       src: 'nograve.PNG',
-      aspectRatio: 4/5,
+      aspectRatio: 4 / 5,
     },
     {
       src: 'promo.PNG',
@@ -26,26 +26,27 @@
     },
     {
       src: 'yfyf.PNG',
-      aspectRatio: 4/5,
+      aspectRatio: 4 / 5,
     },
   ]
   const gallery = document.querySelector('#gallery');
-  
-  images.forEach(({src, aspectRatio}) => {
-    const imgContainer = document.createElement('div');
-    imgContainer.classList.add('item');
 
-    const image = document.createElement('img');
-    image.setAttribute('src', `assets/art/${src}`);
-    image.classList.add('image');
-    imgContainer.appendChild(image);
-
-    const frame = document.createElement('img');
-    frame.setAttribute('src', 'assets/Frame.png');
-    frame.classList.add('frame');
-    imgContainer.appendChild(frame);
-
-    imgContainer.style.aspectRatio = aspectRatio;
-    gallery.appendChild(imgContainer);
+  images.forEach(({ src, aspectRatio }) => {
+    const html = `
+      <div class="item">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img src="assets/art/${src}" class="image" />
+            <img src="assets/Frame.png" class="frame" />
+          </div>
+          <div class="flip-card-back">
+            <h1>John Doe</h1>
+            <p>Architect & Engineer</p>
+            <p>We love that guy</p>
+          </div>
+        </div>
+      </div>
+    `
+    gallery.insertAdjacentHTML('beforeend', html);
   });
 })();
