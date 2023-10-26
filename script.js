e37288c98d0a18f04b054f980d549c8ccaf40df9
Skye Moya <madalynskyee@gmail.com>
2023-10-25 22:28:07 -0400

    Editted textboxes and art gallery

--- script.js
+++ script.js
@@ -2,50 +2,67 @@
   const images = [
     {
       src: 'chop.jpg',
-      aspectRatio: 4/5,
+      aspectRatio: 4 / 5,
+      title: 'Chop',
+      description: 'Description',
     },
     {
       src: 'creep.jpg',
-      aspectRatio: 4/5,
+      aspectRatio: 4 / 5,
+      title: 'Creep',
+      description: '',
     },
     {
-      src: 'iloveme.jpg',
-      aspectRatio: 4/5,
+      src: 'unbroken.jpg',
+      aspectRatio: 4 / 5,
+      title: 'Unbroken',
+      description: 'Coming to terms with past trauma is difficult, and art is cathartic. Pain is subtle but always present.2023',
     },
     {
       src: 'melodrama.jpg',
-      aspectRatio: 4/5,
+      aspectRatio: 4 / 5,
+      title: 'Melodrama',
+      description: 'This piece is what melodrama by Lorde sounds like to me. Drenched in color, full of sadness. 2022',
     },
     {
       src: 'nograve.PNG',
-      aspectRatio: 4/5,
+      aspectRatio: 4 / 5,
+      title: 'No grave',
+      description: 'Work song by Hozier served as an inspiration for this piece. I strive to have the same vigor for myself. 2023 ',
     },
     {
-      src: 'promo.PNG',
-      aspectRatio: 1,
+      src: 'TULIP.JPG',
+      aspectRatio: 4/5,
+      title: 'Unconditional love',
+      description: 'I, like some people, have the honor of having a dog in my life. Chop is my muse. 2023 ',
     },
     {
-      src: 'yfyf.PNG',
-      aspectRatio: 4/5,
+      src: 'attention.PNG',
+      aspectRatio: 4 / 5,
+      title: 'Attention',
+      description: 'My ex-boyfriend gave me this stuffed animal, and it was only fitting to give it to my dog.2023',
     },
   ]
   const gallery = document.querySelector('#gallery');
-  
-  images.forEach(({src, aspectRatio}) => {
-    const imgContainer = document.createElement('div');
-    imgContainer.classList.add('item');
 
-    const image = document.createElement('img');
-    image.setAttribute('src', `assets/art/${src}`);
-    image.classList.add('image');
-    imgContainer.appendChild(image);
-
-    const frame = document.createElement('img');
-    frame.setAttribute('src', 'assets/Frame.png');
-    frame.classList.add('frame');
-    imgContainer.appendChild(frame);
-
-    imgContainer.style.aspectRatio = aspectRatio;
-    gallery.appendChild(imgContainer);
+  images.forEach(({ src, aspectRatio, title, description }) => {
+    // Template literal - this is a bit easier to read than the other way
+    const html = `
+      <div class="item" style="aspect-ratio: ${aspectRatio}">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img src="assets/art/${src}" class="image" />
+            <img src="assets/Frame.png" class="frame" />
+          </div>
+          <div class="flip-card-back">
+            <div class="description">
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+    gallery.insertAdjacentHTML('beforeend', html);
   });
 })();
