2a6d172ee53945d8ae659d4a0345538e7339691a
Skye Moya <madalynskyee@gmail.com>
2023-11-01 21:49:11 -0400

    Added new descriptions and artwork

--- script.js
+++ script.js
@@ -1,47 +1,57 @@
 (() => {
   const images = [
     {
-      src: 'chop.jpg',
-      aspectRatio: 4 / 5,
-      title: 'Chop',
-      description: 'Description',
-    },
-    {
-      src: 'creep.jpg',
-      aspectRatio: 4 / 5,
-      title: 'Creep',
-      description: '',
-    },
-    {
       src: 'unbroken.jpg',
       aspectRatio: 4 / 5,
       title: 'Unbroken',
       description: 'Coming to terms with past trauma is difficult, and art is cathartic. Pain is subtle but always present.2023',
+    
     },
+    { src: 'nograve.PNG',
+      aspectRatio: 4 / 5,
+      title: 'No grave',
+      description: 'Work song by Hozier served as an inspiration for this piece. Meant to convey the light self-love illuminates. 2023 ',
+   
+     
+    },
+    { src: 'halloween.jpg',
+      aspectRatio: 4 / 5,
+      title: 'Happy Halloween',
+      description: 'I created this design for my community project. This young dolly design was given to trick-or-treaters, for Halloween. 2023 ',
+   
+     
+    },
+    {  
+      src: 'attention.PNG',
+      aspectRatio: 4 / 5,
+      title: 'Attention',
+      description: 'My ex-boyfriend gave me this stuffed animal, so I gave it to my dog.2023',
+    
+     },
     {
       src: 'melodrama.jpg',
       aspectRatio: 4 / 5,
       title: 'Melodrama',
       description: 'This piece is what melodrama by Lorde sounds like to me. Drenched in color, full of sadness. 2022',
     },
-    {
-      src: 'nograve.PNG',
+    { 
+      src: 'creep.jpg',
       aspectRatio: 4 / 5,
-      title: 'No grave',
-      description: 'Work song by Hozier served as an inspiration for this piece. I strive to have the same vigor for myself. 2023 ',
-    },
+      title: 'Creep',
+      description: 'Meant to convey the physical manifestation of uncomfort. 2022',
+      },
     {
       src: 'TULIP.JPG',
       aspectRatio: 4/5,
-      title: 'Unconditional love',
-      description: 'I, like some people, have the honor of having a dog in my life. Chop is my muse. 2023 ',
+      title: 'Chop\'s\ Bed',
+      description: 'Chop is my muse. 2023 ',
     },
     {
-      src: 'attention.PNG',
+      src: 'chop.jpg',
       aspectRatio: 4 / 5,
-      title: 'Attention',
-      description: 'My ex-boyfriend gave me this stuffed animal, and it was only fitting to give it to my dog.2023',
-    },
+      title: 'Chop',
+      description: 'My muse (Chop) with a cowboy hat. 2022',
+      },
   ]
   const gallery = document.querySelector('#gallery');
 
